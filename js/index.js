dc62b1c77dfc6ecf5f5b826862a2b8e9582efb9f
Viktoria1209 <63811709+Viktoria1209@users.noreply.github.com>
2020-05-18 16:37:34 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-var countDownDate = new Date("Jun 12, 2020 23:59:00").getTime();
+const countDownDate = new Date("Jun 12, 2020 23:59:00").getTime();
 
-var x = setInterval(function() {
+const x = setInterval(function() {
 
-  var now = new Date().getTime();
+  const now = new Date().getTime();
   var distance = countDownDate - now;
     
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   document.getElementById("timer").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
@@ -23,4 +23,20 @@ document.getElementById("myForm").addEventListener("submit", myFunction);
 
 function myFunction() {
   alert("Thank you for contacting me. Have a nice day!");
+}
+
+const mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
